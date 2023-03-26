b20ca46e1287335103e2937ecfb0e3b0e4585aa7
Abdellah Rachouani <Abdel.Rachouani103@outlook.fr>
2023-03-26 19:11:04 +0200

    first commit

---
+++ resources/views/pages_site/fond.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- LP3MI -->
+<link rel='stylesheet' href='/mon_style.css'>
+@yield('entete')
+<title>
+@yield('titre')
+</title>
+</head>
+<body>
+<div class="container">
+<div class="titre_contenu">
+@yield('titre_contenu')
+</div>
+<div class="contenu">
+@yield('contenu')
+</div>
+<div class="pied_page">@yield('pied_page')</div>
+</div>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function () {
+return view('welcome');
+});
+Route::get('/hello', function () {
+echo '<h1>Bonjour !</h1>';
+});
+Route::get('/bonjour/{nom}', function ($nom) {
+echo "Bonjour " . $nom;
+});
+Route::get('nouvellepage','App\Http\Controllers\MonControleur@retourneNouvellePage');
+Route::get('exemple', 'App\Http\Controllers\MonControleur@retournePageExemple');
+Route::get('membres', 'App\Http\Controllers\ControleurMembres@index');
+
+Route::get('membre/{numero}', 'App\Http\Controllers\ControleurMembres@afficher');
+Route::get('creer', 'App\Http\Controllers\ControleurMembres@creer');
+Route::post('creation/membre', 'App\Http\Controllers\ControleurMembres@enregistrer');
+Route::get('modifier/{id}', 'App\Http\Controllers\ControleurMembres@editer');
+Route::patch('miseAJour/{id}', 'App\Http\Controllers\ControleurMembres@miseAJour');
+
+
+
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/identite','App\Http\Controllers\ControleurMembres@identite');
+Route::get('/protege','App\Http\Controllers\ControleurMembres@acces_protege')
+->middleware('auth');
